feaa43c71ee268a055496e54627e81e38ec7249c
uchiha-satyam <89510414+uchiha-satyam@users.noreply.github.com>
2022-10-17 05:35:38 +0000

    Sync LeetCode submission - Edit Distance (cpp)

--- LeetCode/problems/edit_distance/solution.cpp
+++ LeetCode/problems/edit_distance/solution.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int m = word1.length();
-        int n = word2.length();
-        int dp[m+1][n+1];
-        for(int i=0; i<=m; i++)
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int dp[n1+1][n2+1];
+        for(int i=n1; i>=0; i--)
         {
-            for(int j=0; j<=n; j++)
+            for(int j=n2; j>=0; j--)
             {
-                if(i==0) dp[i][j] = j;
-                else if(j==0) dp[i][j] = i;
-                else if(word1[i-1]==word2[j-1])
-                    dp[i][j] = dp[i-1][j-1];
+                if(i==n1)
+                {
+                    dp[i][j] = n2 - j;
+                    continue;
+                }
+                if(j==n2)
+                {
+                    dp[i][j] = n1 - i;
+                    continue;
+                }
+                if(word1[i]==word2[j])
+                {
+                    dp[i][j] = dp[i+1][j+1];
+                }
                 else
                 {
-                    int a1 = dp[i-1][j];
-                    int a2 = dp[i][j-1];
-                    int a3 = dp[i-1][j-1];
-                    dp[i][j] = 1 + min(a1,min(a2,a3));
+                    dp[i][j] = min(dp[i+1][j+1],min(dp[i+1][j],dp[i][j+1])) + 1;
                 }
             }
         }
-        return dp[m][n];
+        return dp[0][0];
     }
 };
