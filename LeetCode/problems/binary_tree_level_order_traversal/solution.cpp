e9ccf62171afb10c2939f7a063ae60e26002bb67
uchiha-satyam <89510414+uchiha-satyam@users.noreply.github.com>
2022-12-05 05:00:10 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (cpp)

--- LeetCode/problems/binary_tree_level_order_traversal/solution.cpp
+++ LeetCode/problems/binary_tree_level_order_traversal/solution.cpp
@@ -11,46 +11,40 @@
  */
 class Solution {
 public:
-    
-    void levelOrder(queue<vector<TreeNode*>> &q, vector<vector<int>> &v)
-    {
-        if(q.size()==0) return;
-        vector<TreeNode*> qu = q.front(), nqu;
-        vector<int> vec;
-        for(int i=0; i<qu.size(); i++)
-        {
-            TreeNode* root = qu[i];
-            if(root->left)
-            {
-                vec.push_back(root->left->val);
-                nqu.push_back(root->left);
-            }
-            if(root->right)
-            {
-                vec.push_back(root->right->val);
-                nqu.push_back(root->right);
-            }
-        }
-        if(nqu.size())
-        {
-            q.push(nqu);
-            v.push_back(vec);
-        }
-        q.pop();
-        levelOrder(q,v);
-    }
-    
     vector<vector<int>> levelOrder(TreeNode* root) {
-        queue<vector<TreeNode*>> q;
-        vector<TreeNode*> qu;
-        vector<int> vec;
-        vector<vector<int>> v;
-        if(root==NULL) return v;
-        qu.push_back(root);
-        vec.push_back(root->val);
-        q.push(qu);
-        v.push_back(vec);
-        levelOrder(q,v);
-        return v;
+        vector<vector<int>> ans;
+        if(root==NULL) return ans;
+        ans.push_back(vector<int>(1,root->val));
+        queue<TreeNode*> order;
+        order.push(root);
+        order.push(NULL);
+        vector<int> v;
+        while(!order.empty())
+        {
+            TreeNode* node = order.front();
+            order.pop();
+            if(node==NULL)
+            {
+                if(order.empty()) break;
+                else
+                {
+                    order.push(NULL);
+                    ans.push_back(v);
+                    v.clear();
+                    continue;
+                }
+            }
+            if(node->left)
+            {
+                order.push(node->left);
+                v.push_back(node->left->val);
+            }
+            if(node->right)
+            {
+                order.push(node->right);
+                v.push_back(node->right->val);
+            }
+        }
+        return ans;
     }
 };
