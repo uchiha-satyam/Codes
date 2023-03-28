5a031386cf929ed09495c8b5766bee1f29b6aa41
uchiha-satyam <89510414+uchiha-satyam@users.noreply.github.com>
2023-03-28 23:13:39 +0530

    Added solution - LeetHub

---
+++ Possible Words From Phone Digits - GFG/possible-words-from-phone-digits.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    void get(vector<vector<char>>& data, int a[], int& N, int i, string s, vector<string>& ans)
+    {
+        if(i==N)
+        {
+            ans.push_back(s);
+            return;
+        }
+        for(auto& x : data[a[i]])
+        {
+            get(data,a,N,i+1,s+x,ans);
+        }
+    }
+    public:
+    //Function to find list of all words possible by pressing given numbers.
+    vector<string> possibleWords(int a[], int N)
+    {
+        //Your code here
+        vector<string> ans;
+        vector<vector<char>> data = {
+            {},
+            {},
+            {'a','b','c'},
+            {'d','e','f'},
+            {'g','h','i'},
+            {'j','k','l'},
+            {'m','n','o'},
+            {'p','q','r','s'},
+            {'t','u','v'},
+            {'w','x','y','z'}
+        };
+        get(data,a,N,0,"",ans);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+  
+	int T;
+	
+	cin >> T; //testcases
+	
+	while(T--){ //while testcases exist
+	   int N;
+	    
+	   cin >> N; //input size of array
+	   
+	   int a[N]; //declare the array
+	   
+	   for(int i =0;i<N;i++){
+	       cin >> a[i]; //input the elements of array that are keys to be pressed
+	   }
+	   
+	   Solution obj;
+	   
+	  vector <string> res = obj.possibleWords(a,N);
+	  for (string i : res) cout << i << " ";
+	   cout << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
