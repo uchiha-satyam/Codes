8e85ceca814a5ab66334a848c7b19f9f9dfafc3f
uchiha-satyam <89510414+uchiha-satyam@users.noreply.github.com>
2023-04-04 15:42:38 +0530

    Added solution - LeetHub

---
+++ Special Stack - GFG/special-stack.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<iostream>
+#include<stack>
+using namespace std;
+void push(stack<int>& s,int a);
+bool isFull(stack<int>& s,int n);
+bool isEmpty(stack<int>& s);
+int pop(stack<int>& s);
+int getMin(stack<int>& s);
+//This is the STL stack (http://quiz.geeksforgeeks.org/stack-container-adaptors-the-c-standard-template-library-stl/).
+stack<int> s;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,a;
+		cin>>n;
+		while(!isEmpty(s)){
+		    pop(s);
+		}
+		while(!isFull(s,n)){
+			cin>>a;
+			push(s,a);
+		}
+		cout<<getMin(s)<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+void push(stack<int>& s, int a){
+	// Your code goes here
+	s.push(a);
+}
+
+bool isFull(stack<int>& s,int n){
+	// Your code goes here
+	if(s.size()==n) return true;
+}
+
+bool isEmpty(stack<int>& s){
+	// Your code goes here
+	return s.empty();
+}
+
+int pop(stack<int>& s){
+	// Your code goes here
+	s.pop();
+}
+
+int getMin(stack<int>& s){
+	// Your code goes here
+	stack<int> st;
+	int m = 1e9+7;
+	while(!s.empty())
+	{
+	    m = min(m,s.top());
+	    st.push(s.top());
+	    s.pop();
+	}
+	while(!st.empty())
+	{
+	    s.push(st.top());
+	    st.pop();
+	}
+	return m;
+}
