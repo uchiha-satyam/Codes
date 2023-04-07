088585aa307200a472c1fdc15da1f22736207bfc
Satyam Abhishek <89510414+uchiha-satyam@users.noreply.github.com>
2023-04-08 02:07:42 +0530

    Added solution - LeetHub

---
+++ BBT counter - GFG/bbt-counter.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int countBT(int h) { 
+        // code here
+        long long dp[h+1] = {0};
+        long long mod = 1e9+7;
+        dp[0] = dp[1] = 1;
+        for(int i=2; i<=h; i++)
+        {
+            dp[i] = ((dp[i-1]*dp[i-1])%mod + (2*dp[i-1]*dp[i-2])%mod)%mod;
+        }
+        return dp[h];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int h;
+        cin >> h;
+        Solution ob;
+        cout<<ob.countBT(h)<<endl;  
+    }
+    return 0;
+}
+
+// } Driver Code Ends
