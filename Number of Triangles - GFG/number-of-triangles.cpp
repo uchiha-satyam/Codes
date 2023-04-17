3b2808f6b9634a45901ed07f99ab05d050959c31
Satyam Abhishek <89510414+uchiha-satyam@users.noreply.github.com>
2023-04-18 00:00:53 +0530

    Added solution - LeetHub

---
+++ Number of Triangles - GFG/number-of-triangles.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+        int numberOfTriangles(vector<vector<int>> &g,int n)
+        {
+            // code it up
+            int count = 0;
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    if(g[i][j]!=1||i==j) continue;
+                    for(int k=0; k<n; k++)
+                    {
+                        if(i==k||j==k) continue;
+                        if(g[i][j]==g[j][k]&&g[i][j]==g[k][i]) count++;
+                    }
+                }
+            }
+            return count/3;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int>> g(n,vector<int> (n));
+        for(auto &j:g)
+            for(auto &i:j)
+                cin>>i;
+        Solution s;
+        cout<<s.numberOfTriangles(g,n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
