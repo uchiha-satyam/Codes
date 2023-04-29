81dafbd06b0189a1b558f05e7eee1a4446a63532
Satyam Abhishek <89510414+uchiha-satyam@users.noreply.github.com>
2023-04-29 10:12:31 +0530

    Added solution - LeetHub

---
+++ Find Number - GFG/find-number.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    long long findNumber(long long N){
+        // Code here
+        if(N<6)
+        return 2*(N-1)+1;
+        return findNumber((N-1)/5)*10 + 2*((N-1)%5) + 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long N;
+        cin>>N;
+        Solution ob;
+        cout<<ob.findNumber(N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
