ff9ad7668f5f8d97cd2b013e9177fe3502b1b106
Satyam Abhishek <89510414+uchiha-satyam@users.noreply.github.com>
2023-05-27 11:41:44 +0530

    Added solution - LeetHub

---
+++ Modify Linked List-1 - GFG/modify-linked-list1.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node* modifyTheList(struct Node *head);
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void print(Node *head)
+{
+    Node *temp=head;
+	while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+class Solution{
+    public:
+    struct Node* modifyTheList(struct Node *head)
+    {
+        //add code here.
+        stack<Node*> st1, st2;
+        Node* temp = head;
+        while(temp)
+        {
+            st1.push(temp);
+            temp = temp->next;
+        }
+        while(st2.size()<st1.size())
+        {
+            st2.push(st1.top());
+            st1.pop();
+        }
+        if(st2.size()>st1.size()) st2.pop();
+        while(!st1.empty())
+        {
+            int t = st1.top()->data;
+            st1.top()->data = st2.top()->data - t;
+            st2.top()->data = t;
+            st1.pop();
+            st2.pop();
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		struct Node *head = NULL,*temp=NULL;
+		while(n--){
+		    int a;
+            cin>>a;
+			Node *newNode = new Node(a);
+			if(head==NULL)
+                head=newNode;
+            else
+                temp->next=newNode;
+            temp=newNode;
+		}
+		Solution obj;
+		head = obj.modifyTheList(head);
+		print(head);
+		
+	}
+    return 0;
+}
+// } Driver Code Ends
