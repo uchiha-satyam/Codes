8cd53042d73a03f692a995582cb66113dee7b093
Satyam Abhishek <89510414+uchiha-satyam@users.noreply.github.com>
2023-07-10 10:39:45 +0530

    Time: 249 ms (48.26%), Space: 23.8 MB (98.07%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
         for(int i=0; i<n; i++)
         {
-            if(i>0 && nums[i]==nums[i-1]) 
-                continue;
-            int l=i+1;
-            int r=n-1;
-            while(l<r){
-                if(nums[l]+nums[r]+nums[i]==0)
+            if(i!=0&&nums[i]==nums[i-1]) continue;
+            int l = i+1, r = n-1;
+            while(l<r)
+            {
+                int s = nums[l] + nums[r] + nums[i];
+                if(s==0)
                 {
-                    vector<int> t = {nums[i],nums[l],nums[r]};
-                    ans.push_back(t);
+                    ans.push_back({nums[i],nums[l],nums[r]});
+                    while(l<n-1&&nums[l+1]==nums[l]) l++;
+                    while(r>0&&nums[r-1]==nums[r]) r--;
                     l++;
                     r--;
-                    while(l<r && nums[l]==nums[l-1]) 
-                        l++;
-                    while(l<r && nums[r]==nums[r+1]) 
-                        r--;
                 }
-                else if(nums[l]+nums[r]+nums[i]>0)
+                else if(s<0)
                 {
-                    r--;
-                    while(l<r && nums[r]==nums[r+1]) 
-                        r--;
+                    while(l<n-1&&nums[l+1]==nums[l]) l++;
+                    l++;
                 }
                 else
                 {
-                    l++;
-                    while(l<r && nums[l]==nums[l-1]) 
-                        l++;
+                    while(r>0&&nums[r-1]==nums[r]) r--;
+                    r--;
                 }
             }
         }
