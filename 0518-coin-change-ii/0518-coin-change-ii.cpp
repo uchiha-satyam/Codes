ffe85a7517ed0f853c738d73ef5c4e07ceb5231e
Satyam Abhishek <89510414+uchiha-satyam@users.noreply.github.com>
2023-07-17 07:54:04 +0530

    Time: 18 ms (67.19%), Space: 12 MB (74.31%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,33 +1,22 @@
 class Solution {
-    int help(vector<int>& coins, int amount, int idx, int& n, vector<vector<int>>& dp)
-    {
-        if(amount==0) return 1;
-        int ans = 0;
-        if(dp[amount][idx]!=-1) return dp[amount][idx];
-        for(int i=idx; i<n; i++)
-        {
-            if(amount-coins[i]<0) break;
-            ans += help(coins,amount-coins[i],i,n,dp);
-        }
-        return dp[amount][idx] = ans;
-    }
 public:
     int change(int amount, vector<int>& coins) {
-        sort(coins.begin(),coins.end());
         int n = coins.size();
-        vector<vector<int>> dp(amount+1,vector<int>(n,0));
-        // return help(coins,amount,0,n,dp);
+        sort(coins.begin(),coins.end());
+        int dp[amount+1][n];
+        memset(dp,0,sizeof(dp));
         for(int i=0; i<n; i++)
-            dp[0][i] = 1;
+        dp[0][i] = 1;
         for(int i=1; i<=amount; i++)
         {
-            for(int j=n-1; j>=0; j--)
+            for(int j=0; j<n; j++)
             {
-                if(i-coins[j]<0) continue;
+                if(i-coins[j]<0) break;
                 dp[i][j] = dp[i-coins[j]][j];
-                if(j!=n-1) dp[i][j] += dp[i][j+1];
             }
+            for(int j=1; j<n; j++)
+            dp[i][j] += dp[i][j-1];
         }
-        return dp[amount][0];
+        return dp[amount][n-1];
     }
 };
