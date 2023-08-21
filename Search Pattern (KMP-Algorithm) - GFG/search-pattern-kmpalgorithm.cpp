98039a84a9f44b2e7f61b92bf66250c8128f8a9f
Satyam Abhishek <89510414+uchiha-satyam@users.noreply.github.com>
2023-08-21 19:27:19 +0530

    Added solution - LeetHub

--- Search Pattern (KMP-Algorithm) - GFG/search-pattern-kmpalgorithm.cpp
+++ Search Pattern (KMP-Algorithm) - GFG/search-pattern-kmpalgorithm.cpp
@@ -6,55 +6,55 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-    public:
-        vector<int> search(string pat, string txt)
+        void makePi(vector<int>& pi, string& pat)
         {
-            //code here.
-            vector<int> ans;
-            int l1 = txt.length(), l2 = pat.length();
-            vector<int> lps(l2+1,0);
-            for(int i=1; i<=l2; i++)
+            int n = pat.size();
+            for(int i=1; i<n; i++)
             {
-                if(pat[i-1]==pat[lps[i-1]]&&i-1!=lps[i-1])
-                {
-                    lps[i] = lps[i-1]+1;
-                }
+                int j = pi[i-1];
+                while(j!=pi[j]&&pat[i]!=pat[j])
+                j = pi[j];
+                if(pat[i]==pat[j])
+                pi[i] = j+1;
             }
-            // for(int i=0; i<=l2; i++)
-            // {
-            //     cout<<lps[i]<<" ";
-            // }
-            // cout<<endl;
-            int i=0, j=0;
-            while(i<l1)
+        }
+        void kmp(vector<int>& pi, string& pat, string& txt, vector<int>& ans)
+        {
+            int n = txt.size(), m = pat.size();
+            int i = 0, j = 0;
+            while(i<n)
             {
                 if(txt[i]==pat[j])
                 {
                     i++;
                     j++;
+                    if(j==m)
+                    {
+                        ans.push_back(i-m+1);
+                        j--;
+                        j = pi[j];
+                    }
                 }
                 else
                 {
-                    if(j==0)
-                    {
-                        i++;
-                    }
-                    else
-                    {
-                        j = lps[j];
-                    }
-                }
-                // cout<<i<<"->"<<j<<" ";
-                if(j==l2)
-                {
-                    // cout<<i<<" ";
-                    ans.push_back(i-l2+1);
-                    j = lps[j];
+                    if(j!=pi[j]) j = pi[j];
+                    else i++,j=0;
                 }
             }
-            // cout<<endl;
+        }
+    public:
+        vector<int> search(string pat, string txt)
+        {
+            //code here.
+            int n = txt.size(), m = pat.size();
+            vector<int> pi(m,0);
+            makePi(pi,pat);
+            // return pi;
+            vector<int> ans;
+            kmp(pi,pat,txt,ans);
             return ans;
         }
+     
 };
 
 //{ Driver Code Starts.
